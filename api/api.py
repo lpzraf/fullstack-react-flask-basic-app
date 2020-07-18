f5fffcdb0f446130d7c023069e5cbb325fd809e0
Rafael Lopez <lpz.raf@gmail.com>
2020-07-18 18:39:11 -0400

    first commit

--- api/api.py
+++ api/api.py
@@ -1,9 +1,11 @@
-from flask import flask
+# from time import time, ctime
 import time
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/time')
-
 def get_current_time():
+    # t = time()
+    # return {'time': ctime(t)}
     return {'time': time.time()}
